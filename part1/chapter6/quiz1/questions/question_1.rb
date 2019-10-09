0538d751d44ae83629ced91cc911b82da69db8b6
Migle Kubiliute <migle.kubiliute@gmail.com>
2019-10-09 15:24:59 +0100

    give me a number challenge

--- part1/chapter6/quiz1/questions/question_1.rb
+++ part1/chapter6/quiz1/questions/question_1.rb
@@ -18,7 +18,6 @@
 #   stop
 #   18
 #   ```
-
 # * Note: When you run the automated tests, the tests will simulate
 #   the user input.  You shouldn't need to enter any input manually.
 #   If the tests hang when you run them, it probably means your code
@@ -26,4 +25,47 @@
 
 # * Note: You can assume that the user will always enter either an
 #   integer or `stop`.
+# total_names = []
 
+# array = []
+
+#  puts "Give me your name"
+#  while name = gets.chomp do
+# array << name
+#     puts "Give me your name"
+#     if name == "stop"
+#         break
+#     end
+# end
+# puts array
+
+total = 0
+puts "Give me a number "
+while number = gets.chomp do
+    if number == "stop"
+        break
+    end
+    total += number.to_i
+    puts "Give me your number"
+end
+   puts total 
+    
+    
+
+
+
+# x = 4  
+# while true do
+#   puts x
+#  x = x - 1  
+#  if x == 1
+#     break
+# end
+# end
+
+
+
+
+
+    
+    
